c3e73df4ced30e51b2ba91da975682e180e5803a
justMe <justMe@BUGWORM>
2017-03-06 03:37:07 +0200

    Добавьте файлы проекта.

---
+++ Borderlands2Patcher/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Borderlands2Patcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Borderlands2.exe";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Patch Borderlands2.exe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(56, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add Console Hotkey";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "F6";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(56, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Help";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 162);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(119, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Community Patch Video";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(214, 162);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(58, 13);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Changelog";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(56, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(176, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Copy Patch to Binaries Directory";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 184);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Borderlands 2 Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ Borderlands2Patcher/Form1.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Borderlands2Patcher
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //RegistryKey InstallLocation = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 49520");
+        RegistryKey InstallLocation = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 49520");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = new DialogResult();
+            try
+            {
+                string path = InstallLocation.GetValue("InstallLocation") as string;
+                openFileDialog1.FileName = path + "\\Binaries\\Win32\\Borderlands2.exe";
+                result = DialogResult.OK;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Cannot detect your game path. Please select Borderlands2.exe file. It's usually on ...\\Steam(Library)\\steamapps\\common\\Borderlands 2\\Binaries\\Win32 directory.");
+                openFileDialog1.InitialDirectory = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Borderlands 2\\Binaries\\Win32";
+                openFileDialog1.Filter = "Executable files (.exe)|*.exe|All Files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 1;
+                result = openFileDialog1.ShowDialog();
+            }
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    File.Copy(openFileDialog1.FileName, openFileDialog1.FileName + ".bk", false);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("You already have a backup. Skipping.");
+                }
+                var stream = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.ReadWrite);
+                stream.Position = 0x004F2590;
+                stream.WriteByte(0xff);
+                for (long i = 0x01B94B0C; i <= 0x01B94B10; i++)
+                {
+                    stream.Position = i;
+                    stream.WriteByte(0x00);
+                }
+                stream.Close();
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\my games\\borderlands 2\\willowgame\\Config\\WillowInput.ini";
+            string[] temp = File.ReadAllLines(path);
+            int i;
+            for (i = 1; i <= temp.Length; i++)
+            {
+                if (temp[i].StartsWith("ConsoleKey="))
+                    break;
+            }
+            temp[i] = "ConsoleKey=" + textBox1.Text;
+            File.WriteAllLines(path, temp);
+            MessageBox.Show("Done!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To use Community Patch you need to patch your borderlands2.exe (it will backup original file, don't worry), add console hotkey if you doesn't have one and place patch file(patch.txt) to Binaries directory. Program will find your game path and download latest patch automaticly, if won't find it, you still can choose path manually. Then launch your game, open console, type \"exec patch.txt\" and press enter. Do it after game downloads all stuff(when you see actual menu). That's all, you can now enjoy patch! It will work only for current session, you need to enter console command every time you launch the game. You can press \"Arrow Up\" to show your last typed command on console.");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://youtu.be/o_ee3BM1TQQ");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel2.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/AnotherBugworm/Borderlands2Patcher/blob/master/Patch%20Notes.txt");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var file = GetFileViaHttp(@"https://raw.githubusercontent.com/AnotherBugworm/Borderlands2Patcher/master/Patch.txt");
+            string str = Encoding.UTF8.GetString(file);
+            string[] content = str.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                string path = InstallLocation.GetValue("InstallLocation") as string;
+                File.WriteAllLines(path + "\\Binaries\\patch.txt", content);
+                MessageBox.Show("Done!");
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Cannot detect your game path. Choose your Binaries directory. It's usually on ...\\Steam(Library)\\steamapps\\common\\Borderlands 2\\Binaries");
+                DialogResult result = new DialogResult();
+                result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    string path = folderBrowserDialog1.SelectedPath;
+                    File.WriteAllLines(path + "\\patch.txt", content);
+                    MessageBox.Show("Done!");
+                }
+            }
+        }
+
+        public byte[] GetFileViaHttp(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return client.DownloadData(url);
+            }
+        }
+
+    }
+}
